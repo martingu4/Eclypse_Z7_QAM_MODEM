0636f730f28ed568947c021cb3fdfb962a88d922
martingu4 <guillaume.martin4@outlook.com>
2023-08-15 18:08:15 +0200

    feat : Instantiate the developed modules in the modulator top level.

--- Sources/Modulator/Modulator.vhd
+++ Sources/Modulator/Modulator.vhd
@@ -46,30 +46,79 @@ architecture Behavioral of Modulator is
     ----------------------------------------------------------------------
     -- Components declaration
     ----------------------------------------------------------------------
-    -- RNG module
+    -- Symbol rate generator (CE generator for the RNG module)
+    component SymRateGen is
+    generic(
+        PRESCALE_FACTOR : integer := 12000
+    );
+    port(
+        clk         : in  std_logic;
+        resetn      : in  std_logic;
+        sym_ce      : out std_logic
+    );
+    end component SymRateGen;
+
+    -- Random number generator (symbols generation)
     component RNG is
     port (
         clk         : in std_logic;
         resetn      : in std_logic;
+        sym_ce      : in std_logic;
         rand_data   : out std_logic_vector(1 downto 0)
         );
     end component RNG;
+
+    -- Mapper (symbols => I/Q)
+    component Mapper is
+    port (
+        clk         : in std_logic;
+        resetn      : in std_logic;
+        symbol      : in std_logic_vector(1 downto 0);
+        I           : out std_logic_vector(15 downto 0);
+        Q           : out std_logic_vector(15 downto 0)
+    );
+    end component Mapper;
     
     
     
     ----------------------------------------------------------------------
     -- Signals declaration
     ----------------------------------------------------------------------
+    signal sym_ce           : std_logic := '0';
     signal symbols  : std_logic_vector(1 downto 0) := "00";
+    signal I                : std_logic_vector(15 downto 0);
+    signal Q                : std_logic_vector(15 downto 0);
     
 begin
+
+    -- Symbol rate generator module instantiation
+    SymRateGen_inst : SymRateGen
+    generic map(
+        PRESCALE_FACTOR => 10
+    )
+    port map(
+        clk         => clk,
+        resetn      => resetn,
+        sym_ce      => sym_ce
+    );
 
     -- RNG module instantiation
     RNG_inst : RNG
     port map(
         clk         => clk,
         resetn      => resetn,
-        rand_data   => symbols
+        sym_ce      => sym_ce,
+        rand_data   => rand_data
+    );
+
+    -- Mapper module instantiation
+    Mapper_inst : Mapper
+    port map(
+        clk         => clk,
+        resetn      => resetn,
+        symbol      => rand_data,
+        I           => I,
+        Q           => Q
     );
 
 end Behavioral;
